3a20a2bafe3b1407dc31dd6f3a9b127cfc2143b3
Thomas Boyt <thomas.boyt@venmo.com>
2014-03-11 17:45:46 -0400

    Platform generation 1.0

--- src/platform_controller.js
+++ src/platform_controller.js
@@ -1,7 +1,7 @@
 import Platform from './platform';
 
 var PlatformController = function(game, settings) {
-  this.scrollSpeed = 2;
+  this.scrollSpeed = 5;
 
   this.platforms = [];
 
@@ -39,6 +39,69 @@ var PlatformController = function(game, settings) {
   ];
 };
 
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+PlatformController.prototype.generatePlatform = function() {
+  var height = 25;
+  var centerY = 0 - (height / 2);
+
+  // types of platforms:
+  // hole       |========   ======|
+  // left edge  |===              |
+  // right edge |              ===|
+  // center     |     =======     |
+
+  var types = {HOLE: 0, LEFT_EDGE: 1, RIGHT_EDGE: 2, CENTER: 3};
+
+  var type = getRandomInt(0, 3);
+
+  var platforms;
+  var width;
+
+  if (type === types.HOLE) {
+    // find hole width
+    width = getRandomInt(50, 450);
+    platforms = [
+      c.entities.create(Platform, {
+        center: {x: (500 - width) / 4 , y: centerY},
+        size: {x: (500 - width) / 2,  y: height}
+      }),
+      c.entities.create(Platform, {
+        center: {x: 500 - (500 - width) / 4, y: centerY},
+        size: {x: (500 - width) / 2, y: height}
+      }),
+    ];
+  } else if (type === types.LEFT_EDGE) {
+    width = getRandomInt(50, 450);
+    platforms = [
+      c.entities.create(Platform, {
+        center: {x: width / 2, y: centerY},
+        size: {x: width, y: height}
+      })
+    ];
+  } else if (type === types.RIGHT_EDGE) {
+    width = getRandomInt(50, 450);
+    platforms = [
+      c.entities.create(Platform, {
+        center: {x: 500 - (width / 2), y: centerY},
+        size: {x: width, y: height}
+      })
+    ];
+  } else if (type === types.CENTER) {
+    width = getRandomInt(50, 450);
+    platforms = [
+      c.entities.create(Platform, {
+        center: {x: 250, y: centerY},
+        size: {x: width, y: height}
+      })
+    ];
+  }
+
+  this.platforms = this.platforms.concat(platforms);
+};
+
 PlatformController.prototype.update = function(dt) {
   var step = dt/100;
 
@@ -52,6 +115,8 @@ PlatformController.prototype.update = function(dt) {
     if (top > 500) {
       c.entities.destroy(platform);
       this.platforms.splice(i, 1);
+
+      this.generatePlatform();
     }
   }
 
